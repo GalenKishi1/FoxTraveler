a13d829dceff9b900e12a04775da35b7bc11c16c
GalenKishi1 <zap105@amerike.edu.mx>
2023-04-07 17:15:27 -0600

    ActualizacionDePostProcesing

--- ProjectFoxTraveler/Assets/_Scripts/GameManager/GameManager.cs
+++ ProjectFoxTraveler/Assets/_Scripts/GameManager/GameManager.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
+    public GameObject Credits;
+    public GameObject[] TextAndButtons;
 
     void Start()
     {
@@ -18,5 +20,20 @@ public class GameManager : MonoBehaviour
     public void LoadSceneGamePlay()
     {
         SceneManager.LoadScene("Gameplay", LoadSceneMode.Single);
+    }
+
+    public void UseCredits()
+    {
+        Credits.gameObject.SetActive(true);
+        for (int counting = 0; counting <= TextAndButtons.Length; counting++)
+        {
+
+            TextAndButtons[counting].SetActive(true);
+        }
+    }
+
+    IEnumerator ShowButtonsAndText()
+    {
+        yield return new WaitForSeconds(1f); 
     }
 }
